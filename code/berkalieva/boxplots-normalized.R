ac9b105d6a67d2e9718cd568a18a53e6693815e4
Asem Berkalieva <aberkaliev@gmail.com>
2018-11-29 15:49:53 -0800

    more work with exploring normalization

---
+++ code/berkalieva/boxplots-normalized.R
@@ -0,0 +1,39 @@
+
+# look at the PROBESETS
+par(mfrow=c(2,3))
+boxplot(non_normalized,
+        main = "Non-normalized")
+boxplot(normalized_quantile,
+        main = "Normalized Quantile")
+boxplot(t(t_normalized_quantile),
+        main = "t-Normalized Quantile")
+boxplot(normalized_loess,
+        main = "Normalized LOESS")
+boxplot(normalized_scale,
+        main = "Normalized Scale")
+boxplot(genes)
+
+
+# look at CUT_1
+par(mfrow=c(2,3))
+boxplot(non_norm_cut,
+        main = "Non-normalized")
+boxplot(cut_quantile,
+        main = "Normalized Quantile")
+boxplot(t(t_cut_quantile),
+        main = "t-Normalized Quantile")
+boxplot(cut_loess,
+        main = "Normalized LOESS")
+boxplot(cut_scale,
+        main = "Normalized Scale")
+boxplot(genes)
+
+
+par(mfrow=c(1,2))
+boxplot(t(non_norm_cut),
+        main = "Non-normalized")
+boxplot(t(genes),
+        main = "FigShare Sample")
+
+
+
